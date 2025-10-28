f56446b3356cb53652c032b819802a611ea0caf0
Carles Capellas <capellas.carles@gmail.com>
2025-10-28 13:33:58 +0100

    Support csv tables format

--- src/logic/table.logic.ts
+++ src/logic/table.logic.ts
@@ -1,12 +1,27 @@
+export const tableFormat: 'csv' | 'markdown' = 'markdown';
+
 export const getTable = (headers: (string | number)[], rows: (string | number)[][]) => {
-  return [headersToMarkdown(headers), ...rows.map((row) => rowToMarkdown(row))].join('\n');
+  const lines =
+    tableFormat === 'markdown'
+      ? [...headersToMarkdown(headers), ...rows.map((row) => rowToMarkdown(row))]
+      : [headersToCsv(headers), ...rows.map((row) => rowToCsv(row))];
+
+  return lines.join('\n');
+};
+
+const headersToCsv = (headers: (string | number)[]) => {
+  return `${headers.join(',')}`;
 };
 
 const headersToMarkdown = (headers: (string | number)[]) => {
   const headersRow = headers.map((header) => `| ${header} `).join('') + '|';
   const separatorRow = headers.map(() => '| --- ').join('') + '|';
 
-  return [headersRow, separatorRow].join('\n');
+  return [headersRow, separatorRow];
+};
+
+const rowToCsv = (columns: (string | number)[]) => {
+  return columns.map((column) => (String(column).includes(',') ? `"${column}"` : column)).join(',');
 };
 
 const rowToMarkdown = (columns: (string | number)[]) => {
