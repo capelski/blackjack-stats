b1abf334005d03d778461834f0d0b97c6ca6368d
Carles Capellas <capellas.carles@gmail.com>
2026-02-08 09:24:29 +0100

    Merge misleading files

--- src/logic/strategies/dealer-card.logic.ts
+++ src/logic/strategies/dealer-card.logic.ts
@@ -1,13 +1,37 @@
+import { DealerCardStrategy } from '../../types/dealer-card-strategy.type';
+import { FinalScoresByDealerCard } from '../../types/final-scores.type';
 import { StrategyOptions } from '../../types/strategy-options.type';
 import { cards } from '../cards.logic';
-import {
-  createDealerCardStrategy,
-  setDealerCardStrategyTotals,
-} from '../dealer-card-strategy.logic';
 import { getDealerFinalsByCard } from '../dealer-finals-by-card.logic';
+import { getDealerFinals } from '../dealer-finals.logic';
 import { getFinalProbabilities } from '../final-scores.logic';
+import {
+  createPlayerDecisionStrategy,
+  mergePlayerDecisionStrategies,
+  setPlayerDecisionStrategyTotals,
+} from '../player-decision-strategy.logic';
+import { createStrategySummary, getStrategySummary } from '../strategy-summary.logic';
 import { printDealerCardStrategyTables } from '../table.logic';
 import { maxReturnsCore } from './max-returns.logic';
+
+const createDealerCardStrategy = (
+  dealerFinalsByCard: FinalScoresByDealerCard,
+  options: StrategyOptions = {},
+): DealerCardStrategy => {
+  const strategy: DealerCardStrategy = {
+    dealerCards: cards.reduce((reduced, card) => {
+      return {
+        ...reduced,
+        [card]: createPlayerDecisionStrategy(dealerFinalsByCard[card], options),
+      };
+    }, {}),
+    dealerFinalScores: getDealerFinals(),
+    options,
+    summary: createStrategySummary(),
+  };
+
+  return strategy;
+};
 
 export const getDealerCardStrategy = (options: StrategyOptions = {}) => {
   const dealerFinalsByCard = getDealerFinalsByCard({ useCardLevelProbabilities: true });
@@ -27,4 +51,17 @@ export const getDealerCardStrategy = (options: StrategyOptions = {}) => {
 export const printDealerCardStrategy = (strategyOptions: StrategyOptions = {}) => {
   const strategy = getDealerCardStrategy(strategyOptions);
   printDealerCardStrategyTables(strategy);
+};
+
+const setDealerCardStrategyTotals = (strategy: DealerCardStrategy) => {
+  Object.keys(strategy.dealerCards).forEach(dealerCard => {
+    const partialStrategy = strategy.dealerCards[dealerCard];
+    setPlayerDecisionStrategyTotals(partialStrategy);
+  });
+
+  const mergedConsequences = mergePlayerDecisionStrategies(Object.values(strategy.dealerCards));
+
+  const overallDealerFinals = getDealerFinals();
+  // TODO Pass playerFinalScores and combinations from combined partial strategies
+  strategy.summary = getStrategySummary(mergedConsequences, overallDealerFinals);
 };
