e16bc07c5654cb3cfe7bf7fc53903ad22cba42fa
Carles Capellas <capellas.carles@gmail.com>
2026-05-10 17:22:38 +0200

    Refine toPercentage function

--- ui/src/logic/numbers.logic.ts
+++ ui/src/logic/numbers.logic.ts
@@ -11,7 +11,7 @@ export const toDecimal = (value: number, decimals = 2) => {
 export function toPercentage(probability: number): string {
   const percentage = probability * 100;
 
-  if (percentage === 0 || Math.abs(percentage) >= 0.01) {
+  if (percentage === 0 || Math.abs(percentage) >= 0.0001) {
     return `${percentage.toFixed(4)}%`;
   }
 
