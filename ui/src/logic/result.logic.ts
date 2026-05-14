f9e381d2d133fba800723fce5d89f2a754efe786
Carles Capellas <capellas.carles@gmail.com>
2026-05-14 15:20:19 +0200

    Replace rgba colors with hex color

--- ui/src/logic/result.logic.ts
+++ ui/src/logic/result.logic.ts
@@ -16,10 +16,10 @@ export const getResult = (playerScore: number, dealerScore: number): Result => {
 
 export const resultToStyles = (result: Result): CSSProperties | undefined => {
   return result === lose
-    ? { backgroundColor: 'rgba(214, 111, 72, 0.2)', color: '#8e2f12' }
+    ? { backgroundColor: '#f7e2db', color: '#8e2f12' }
     : result === push
-    ? { backgroundColor: 'rgba(209, 176, 80, 0.25)', color: '#7b5a05' }
+    ? { backgroundColor: '#f3ebd3', color: '#7b5a05' }
     : result === win
-    ? { backgroundColor: 'rgba(15, 125, 81, 0.18)', color: '#095939' }
+    ? { backgroundColor: '#d9e7e1', color: '#095939' }
     : undefined;
 };
