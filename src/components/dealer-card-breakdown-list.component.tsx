c80483918a2495d89a2499ec4abae29f45a4ef4d
Carles Capellas <capellas.carles@gmail.com>
2026-09-01 14:49:19 +0200

    Remove outcome columns from the dealer card breakdown list

--- src/components/dealer-card-breakdown-list.component.tsx
+++ src/components/dealer-card-breakdown-list.component.tsx
@@ -4,10 +4,8 @@ import { useDealerCardContext } from '../dealer-card.context';
 import { dealerFinalScoresByFirstCard } from '../logic/dealer-data.logic';
 import { toPercentage } from '../logic/numbers.logic';
 import { sortedCardSymbols } from '../models/cards.model';
-import { lose, push, surrender, win } from '../models/result.model';
 import { useSearchParamsUtils } from '../search-params-utils';
 import { useSettingsContext } from '../settings.context';
-import { BetMultipliersCell } from './bet-multipliers-cell.component';
 
 const getCellStyle = (isHeader = false): CSSProperties => ({
   fontWeight: isHeader ? 'bold' : undefined,
@@ -18,14 +16,12 @@ const getCellStyle = (isHeader = false): CSSProperties => ({
 export const DealerCardBreakdownList: React.FC = () => {
   const { t } = useTranslation();
   const { decimals } = useSettingsContext();
-  const { rules, strategy } = useDealerCardContext();
+  const { strategy } = useDealerCardContext();
   const { navigateWithSearch } = useSearchParamsUtils();
-
-  const surrenderingEnabled = !!rules.surrendering;
 
   const rowStyle: CSSProperties = {
     display: 'grid',
-    gridTemplateColumns: `repeat(${7 + +surrenderingEnabled}, 1fr)`,
+    gridTemplateColumns: 'repeat(4, 1fr)',
   };
 
   return (
@@ -35,17 +31,13 @@ export const DealerCardBreakdownList: React.FC = () => {
           <tr style={rowStyle}>
             <td style={getCellStyle(true)}>{t('titles.dealerCard')}</td>
             <td style={getCellStyle(true)}>{t('commons.probability')}</td>
-            <td style={getCellStyle(true)}>{t(`commons.win`)}</td>
-            <td style={getCellStyle(true)}>{t(`commons.push`)}</td>
-            <td style={getCellStyle(true)}>{t(`commons.lose`)}</td>
-            {surrenderingEnabled && <td style={getCellStyle(true)}>{t(`commons.surrender`)}</td>}
             <td style={getCellStyle(true)}>{t('commons.edge')}</td>
             <td style={getCellStyle(true)}></td>
           </tr>
         </thead>
 
         <tbody>
-          {sortedCardSymbols.map(cardSymbol => {
+          {sortedCardSymbols.map((cardSymbol) => {
             const cardStrategy = strategy.breakdown[cardSymbol];
             const probability = dealerFinalScoresByFirstCard[cardSymbol]?.probability ?? 0;
 
@@ -53,26 +45,12 @@ export const DealerCardBreakdownList: React.FC = () => {
               return null;
             }
 
-            const { edge, outcomesByBetMultiplier } = cardStrategy.expectedResults;
+            const { edge } = cardStrategy.expectedResults;
 
             return (
               <tr key={cardSymbol} style={rowStyle}>
                 <td style={getCellStyle(true)}>{cardSymbol}</td>
                 <td style={getCellStyle()}>{toPercentage(probability, decimals)}</td>
-                <td style={getCellStyle()}>
-                  <BetMultipliersCell map={outcomesByBetMultiplier[win]} />
-                </td>
-                <td style={getCellStyle()}>
-                  <BetMultipliersCell map={outcomesByBetMultiplier[push]} />
-                </td>
-                <td style={getCellStyle()}>
-                  <BetMultipliersCell map={outcomesByBetMultiplier[lose]} />
-                </td>
-                {surrenderingEnabled && (
-                  <td style={getCellStyle()}>
-                    <BetMultipliersCell map={outcomesByBetMultiplier[surrender]} />
-                  </td>
-                )}
                 <td style={getCellStyle()}>{toPercentage(edge, decimals)}</td>
                 <td style={getCellStyle()}>
                   <button
